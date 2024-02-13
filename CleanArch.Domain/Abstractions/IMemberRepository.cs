ae42da3f884df91926852a934a4158f50e261987
Renan Monteiro <renanmont.silva@gmail.com>
2024-02-13 18:42:30 -0300

    Commit Inicial

---
+++ CleanArch.Domain/Abstractions/IMemberRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Abstractions
+{
+    public interface IMemberRepository
+    {
+        Task<IEnumerable<Member>> GetMembers();
+        Task<Member> GetMemberById(int id);
+        Task<Member> AddMember(Member member);
+        Task<Member> UpdateMember(Member member);
+        Task DeleteMember(int id);
+    }
+}
---
+++ CleanArch.Domain/Entities/Member.cs
@@ -0,0 +1,68 @@
+﻿using CleanArch.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Entities
+{
+    public sealed class Member : Entity
+    {
+        public string? FirstName { get; private set; }
+        public string? LastName { get; private set; }
+        public string? Gender { get; private set; }
+        public string? Email { get; private set; }
+        public bool? IsActive { get; private set; }
+
+        public Member(string firstName, string lastName, string gender, string email, bool? active)
+        {
+            ValidateDomain(firstName, lastName, gender, email, active);
+        }
+
+        public void Update(string firstName, string lastName, string gender, string email, bool? active)
+        {
+            ValidateDomain(firstName, lastName, gender, email, active);
+        }
+
+        public Member(int id, string firstName, string lastName, string gender, string email, bool? active)
+        {
+            DomainValidation.When(id < 0, "Invalid Id value.");
+            Id = id;
+            ValidateDomain(firstName, lastName, gender, email, active);
+        }
+
+        private void ValidateDomain(string firstName, string lastName, string gender, string email, bool? active)
+        {
+            DomainValidation.When(string.IsNullOrEmpty(firstName), 
+                "Invalid name. FirstName is required");
+
+            DomainValidation.When(firstName.Length < 3, 
+                "Invalid name, too short, minimum 3 characters");
+
+            DomainValidation.When(string.IsNullOrEmpty(lastName), 
+                "Invalid lastname. LastName is required");
+
+            DomainValidation.When(lastName.Length < 3, 
+                "Invalid lastname, too short, minimum 3 characters");
+
+            DomainValidation.When(email?.Length > 250, 
+                "Invalid email, too long, maximum 250 characters");
+
+            DomainValidation.When(email?.Length < 6, 
+                "Invalid email, too short, minimum 6 characters");
+
+            DomainValidation.When(string.IsNullOrEmpty(gender), 
+                "Invalid gender. Gender is required");
+
+            DomainValidation.When(!active.HasValue, 
+                "Must define activity");
+
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Gender = gender;
+            IsActive = active;
+        }
+    }
+}
